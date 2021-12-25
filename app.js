e4e4e2544156bce732b6895c857bc5e5d9c7b2e8
NaHyeon <nahyeonee99@gmail.com>
2021-12-25 11:34:50 +0900

    array

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const amIFat = null;
-let something;
-console.log(amIFat, something);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+const num = [1, 2, "hello", false, 5, null, undefined, "", "nico", 10];
+
+console.log(daysOfWeek[2]);
+console.log(num);
+
+console.log(daysOfWeek);
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+daysOfWeek.pop();
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+console.log(toBuy[999]);
