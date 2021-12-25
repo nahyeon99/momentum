9fe0daa5d6109fcb9f080ed03202da6a18cdc93a
NaHyeon <nahyeonee99@gmail.com>
2021-12-25 13:49:09 +0900

    isNan() 과 조건문

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  multiplay: function (a, b) {
-    return a * b;
-  },
-  devide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-console.log(calculator.add(2, 3));
-console.log(calculator.minus(2, 3));
-console.log(calculator.multiplay(2, 3));
-console.log(calculator.devide(2, 3));
-console.log(calculator.power(2, 3));
+if (isNaN(age)) {
+  console.log("Please write a number");
+} else {
+  console.log("Thank you for writing your age");
+}
