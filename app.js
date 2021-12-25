d468fc4158d14bd15a3359c15cfe114867328c63
NaHyeon <nahyeonee99@gmail.com>
2021-12-25 11:58:04 +0900

    functions

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+// 1
+function sayHello(name, age) {
+  console.log("Hello my name is ", name, " My age is", age);
+}
+
+sayHello("nahyeon", 23);
+
+// 2
+function plus(a, b) {
+  return a + b;
+}
+
+function devide(a, b) {
+  return a / b;
+}
+
+console.log(plus(2, 3));
+console.log(devide(40, 2));
+
+// 3
 const player = {
-  name: "nico",
-  points: 10,
-  fat: true,
+  name: "nahyeon",
+  sayHi: function (name) {
+    console.log("hi ", name, " nice to meet you!");
+  },
 };
 
-console.log(player);
 console.log(player.name);
-console.log(player["name"]);
-
-player.fat = false;
-console.log(player);
-
-player.lastName = "potato";
-console.log(player);
+player.sayHi("hy");
+player.sayHi("hyeon");
