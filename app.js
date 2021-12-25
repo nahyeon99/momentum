5c92bb2ff5fbc9bf0b6a0748a5da4c42feb0e9cb
NaHyeon <nahyeonee99@gmail.com>
2021-12-25 11:24:18 +0900

    datatype(boolean,null,undefined)

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-let a = 10;
-const b = "2";
-let myName = "nico"; // myName은 update 될 수 있다
-const yourName = "hyeon"; // yourName은 update 될 수 없다
-
-console.log(a + b); // 더하기는 문자로 처리 되고
-console.log(a * b); // 나머지 산술연산자는 계산됨.
-console.log(a / b);
-console.log(a - b);
-console.log("hello" + " nico");
-console.log("hello " + myName);
-
-myName = "hyeon";
-console.log(myName);
-// yourName = "nahyeon"; // typeErr
+const amIFat = null;
+let something;
+console.log(amIFat, something);
