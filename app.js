d5205f8e183699f30cd617818ba917c19642fe4e
NaHyeon <nahyeonee99@gmail.com>
2021-12-27 08:30:15 +0900

    CSS in JavaScript3

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
+// function handleClick() {
+//   const clickedClass = "clicked";
+//   if (h1.classList.contains(clickedClass)) {
+//     h1.classList.remove(clickedClass);
+//   } else {
+//     h1.classList.add(clickedClass);
+//   }
+// }
+
+// toggle을 사용하는 경우
 function handleClick() {
-  const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  } else {
-    h1.className = clickedClass;
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleClick);
