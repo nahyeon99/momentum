2847e1059039878dc067ce76a23d886678923e8a
NaHyeon <nahyeonee99@gmail.com>
2021-12-27 07:38:11 +0900

    CSS in JavaScript

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-h1.style.color = "blue";
-
 function handleClick() {
-  console.log("clicked!");
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 function handleMouseEnter() {
   h1.innerText = "Mouse entered!";
@@ -11,28 +16,7 @@ function handleMouseEnter() {
 function handleMouseLeave() {
   h1.innerText = "Mouse is gone!";
 }
-console.dir(h1);
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier");
-}
-function handleWindowOffLine() {
-  alert("SOS no wifi");
-}
-function handleWindowOnLine() {
-  alert("Online!");
-}
-
-// h1.onclick = handleClick;
 
 h1.addEventListener("click", handleClick);
 h1.addEventListener("mouseenter", handleMouseEnter);
 h1.addEventListener("mouseleave", handleMouseLeave);
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffLine);
-window.addEventListener("online", handleWindowOnLine);
