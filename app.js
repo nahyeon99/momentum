880c52896bd892f0d3e930aa82b8439b2977ce33
NaHyeon <nahyeonee99@gmail.com>
2021-12-27 07:07:38 +0900

    events2

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-e.addEventListener("click", handleTitleclick);
-
 const title = document.querySelector("div.hello:first-child h1");
+
 title.style.color = "blue";
-function handleTitleClick() {
-  console.log("title was clicked");
+
+function handleClick() {
+  console.log("clicked!");
 }
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+  title.innerText = "Mouse entered!";
+}
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+console.dir(title);
