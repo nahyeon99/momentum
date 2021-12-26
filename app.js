35e96f3912ecaac4e96711df59c095061bdc2523
NaHyeon <nahyeonee99@gmail.com>
2021-12-27 07:45:53 +0900

    CSS in JavaScript part Two

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
-}
-function handleMouseEnter() {
-  h1.innerText = "Mouse entered!";
-}
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
 }
 
 h1.addEventListener("click", handleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
